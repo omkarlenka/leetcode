22118907359070eb80452ade2632721bfaedc726
omkarlenka <omkar.lenka@gmail.com>
2020-04-18 17:53:55 +0530

    https://leetcode.com/problems/count-complete-tree-nodes/
    https://leetcode.com/problems/count-complete-tree-nodes/

---
+++ count_complete_tree_nodes.cpp
@@ -0,0 +1,27 @@
+//
+//  count_complete_tree_nodes.cpp
+//
+//  Created by omlenka on 18/04/20.
+//  Copyright © 2020 omkar lenka. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+        return 1+countNodes(root->left) + countNodes(root->right);
+    }
+};
