f3ca1adf43f7338115a97886cec6644c76aa0837
omkarlenka <omkar.lenka@gmail.com>
2020-04-25 21:51:06 +0530

    https://leetcode.com/problems/flip-equivalent-binary-trees/
    https://leetcode.com/problems/flip-equivalent-binary-trees/

--- flip_equivalent_binary_trees.cpp
+++ flip_equivalent_binary_trees.cpp
@@ -24,33 +24,10 @@ public:
     {
         if(root1 == NULL && root2 == NULL)
             return true;
-        else if(root1 == NULL || root2 == NULL)
+        else if(root1 == NULL || root2 == NULL || root1->val != root2->val)
             return false;
         
-        if(root1->val == root2->val)
-        {
-            if(root1->left == NULL && root1->right == NULL  && root2->left == NULL  && root2->right == NULL)
-                return true;
-            if(root1->left && root2->left && root1->right && root2->right)
-            {
-                if(root1->left->val == root2->right->val && root1->right->val == root2->left->val)
-                {
-                    return flipEquiv(root1->left,root2->right) && flipEquiv(root1->right, root2->left);
-                }
-                else
-                {
-                    return flipEquiv(root1->left,root2->left) && flipEquiv(root1->right, root2->right);
-                }
-            }
-            else if(root1->left && root2->right && !root1->right && ! root2->left)
-                return flipEquiv(root1->left,root2->right);
-            else if(root1->right && root2->left && !root1->left && !root2->right)
-                return flipEquiv(root1->right, root2->left);
-            else if(root1->left && root2->left && !root1->right && !root2->right)
-                return flipEquiv(root1->left, root2->left);
-            else if(root1->right && root2->right && !root1->left && !root2->left)
-                return flipEquiv(root1->right, root2->right);
-        }
-        return false;
+        return (flipEquiv(root1->left,root2->left) && flipEquiv(root1->right,root2->right)) ||
+        (flipEquiv(root1->right,root2->left) && flipEquiv(root1->left,root2->right));
     }
 };
