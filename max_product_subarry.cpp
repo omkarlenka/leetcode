24c9444eaa7eadade8bf97d1793a1b72615000ab
omkarlenka <omkar.lenka@gmail.com>
2020-05-01 02:39:00 +0530

    https://leetcode.com/problems/maximum-product-subarray
    Memory optimization

--- max_product_subarry.cpp
+++ max_product_subarry.cpp
@@ -17,16 +17,13 @@ public:
         else if(nums.size() == 1)
             return nums[0];
         
-        vector<vector<int>> dp(nums.size());  //negative, positive
-        vector<int> v = {nums[0],nums[0]};
-        dp[0] = v;
-        
+        int n = nums[0];
+        int p = nums[0];
         int max_prod = nums[0];
         for(int i = 1;i<nums.size();i++)
         {
-            int n = 0, p = 0;
-            int x = dp[i-1][0] * nums[i];
-            int y = dp[i-1][1] * nums[i];
+            int x = n * nums[i];
+            int y = p * nums[i];
             if(x == y)
             {
                 if(x == 0)
@@ -54,8 +51,6 @@ public:
                 n = min(n, nums[i]);
             if(nums[i] > 0)
                 p = max(p, nums[i]);
-            vector<int> v = {n,p};
-            dp[i] = v;
             
             if(p > max_prod)
                 max_prod   = p;
