1a533636624612da1eb5251533b40da00633ffd7
omkarlenka <omkar.lenka@gmail.com>
2020-10-24 22:14:53 +0530

    https://leetcode.com/problems/same-tree/
    https://leetcode.com/problems/same-tree/

---
+++ same_tree.cpp
@@ -0,0 +1,31 @@
+//
+//  same_tree.cpp
+//
+//  Created by omlenka on 24/10/20.
+//  Copyright © 2020 omkar lenka. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p && !q)
+            return true;
+        if(!p || !q)
+            return false;
+        return (p->val == q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
+    }
+};
