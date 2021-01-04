1506c3f7ebbeac599bb4c19ecfb30a7a17b2cbe8
omkarlenka <omkar.lenka@gmail.com>
2021-01-04 21:13:17 +0530

    https://leetcode.com/problems/reconstruct-itinerary/
    https://leetcode.com/problems/reconstruct-itinerary/

--- reconstruct_itinerary_lc332.cpp
+++ reconstruct_itinerary_lc332.cpp
@@ -14,16 +14,36 @@
 using namespace std;
 
 class Solution {
+    void findItineraryUtil(unordered_map<string,multiset<string>> &M, string src, int &no_of_tickets, vector<string> &res){
+        res.push_back(src);
+        if(no_of_tickets == 0)
+            return;
+        auto itr = M[src].begin();
+        int i = 0;
+        while(itr != M[src].end()){
+            string dst = *itr;
+            M[src].erase(itr);
+            findItineraryUtil(M, dst, --no_of_tickets, res);
+            if(no_of_tickets == 0)
+                return;
+            //failure, add back edge
+            M[src].insert(dst);
+            no_of_tickets++;
+            i++;
+            itr = next(M[src].begin(),i);
+        }
+        res.pop_back();
+    }
 public:
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        unordered_map<string,set<string>> M;
+        unordered_map<string,multiset<string>> M;
         int no_of_tickets = tickets.size();
         for(vector<string> ticket:tickets){
             string from = ticket[0];
             string to = ticket[1];
             
             if(M.find(from) == M.end()){
-                set<string> S;
+                multiset<string> S;
                 S.insert(to);
                 M[from] = S;
             }
@@ -31,50 +51,23 @@ public:
                 M[from].insert(to);
             }
         }
-        
-        auto itr = M.find("JFK");
-        stack<string> stk;
-        stk.push("JFK");
-        string insert = "";
-        while(no_of_tickets){
-            if(itr != M.end() && !itr->second.empty()){
-                auto set_itr = itr->second.begin();
-                string next_hop = *set_itr;
-                stk.push(next_hop);
-                itr->second.erase(set_itr);
-                if(!insert.empty()){
-                    itr->second.insert(insert);
-                    insert.clear();
-                    no_of_tickets++;
-                }
-                itr = M.find(next_hop);
-                no_of_tickets--;
-            }
-            else{
-                string to = stk.top();
-                stk.pop();
-                string from = stk.top();
-                insert = to;
-                itr = M.find(from);
-            }
-        }
-        
         vector<string> res;
-        while(!stk.empty()){
-            string top = stk.top();
-            stk.pop();
-            res.push_back(top);
-        }
-        reverse(res.begin(),res.end());
+        string start = "JFK";
+        findItineraryUtil(M, start, no_of_tickets, res);
         return res;
     }
 };
 
 int main() {
     Solution S;
-//    vector<vector<string>> tickets = {{"MUC","LHR"},{"JFK","MUC"},{"SFO","SJC"},{"LHR","SFO"}};
-//    vector<vector<string>> tickets = {{"JFK","SFO"},{"JFK","ATL"},{"SFO","ATL"},{"ATL","JFK"},{"ATL","SFO"}};
-    vector<vector<string>> tickets = {{"JFK","KUL"},{"JFK","NRT"},{"NRT","JFK"}};
+    //    vector<vector<string>> tickets = {{"MUC","LHR"},{"JFK","MUC"},{"SFO","SJC"},{"LHR","SFO"}};
+    //    vector<vector<string>> tickets = {{"JFK","SFO"},{"JFK","ATL"},{"SFO","ATL"},{"ATL","JFK"},{"ATL","SFO"}};
+    //    vector<vector<string>> tickets = {{"JFK","KUL"},{"JFK","NRT"},{"NRT","JFK"}};
+    //    vector<vector<string>> tickets = {{"JFK","DEF"},{"JFK","MNO"},{"MNO","JFK"},{"DEF","PQR"}};
+    //    vector<vector<string>> tickets = {{"EZE","AXA"},{"TIA","ANU"},{"ANU","JFK"},{"JFK","ANU"},{"ANU","EZE"},{"TIA","ANU"},{"AXA","TIA"},{"TIA","JFK"},{"ANU","TIA"},{"JFK","TIA"}};
+    //    vector<vector<string>> tickets = {{"MEL","PER"},{"SYD","CBR"},{"AUA","DRW"},{"JFK","EZE"},{"PER","AXA"},{"DRW","AUA"},{"EZE","SYD"},{"AUA","MEL"},{"DRW","AUA"},{"PER","ANU"},{"CBR","EZE"},{"EZE","PER"},{"MEL","EZE"},{"EZE","MEL"},{"EZE","TBI"},{"ADL","DRW"},{"ANU","EZE"},{"AXA","ADL"}};
+    //    vector<vector<string>> tickets = {{"JFK","ABC"},{"ABC","PQR"},{"PQR","ABC"},{"ABC","MNO"}};
+    vector<vector<string>> tickets = {{"JFK","DEF"},{"JFK","MNO"},{"MNO","ABC"},{"DEF","PQR"}};
     vector<string> res = S.findItinerary(tickets);
     return 0;
 }
